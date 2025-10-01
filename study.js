bc8c45729693dec3895fcefed0b6373cb8d6fff5
Jung20000 <jhpaxk02@naver.com>
2025-10-01 16:43:55 +0900

    docs: JavaScript - statement and expression

--- study.js
+++ study.js
@@ -1,10 +1,6 @@
-function printRank(first, second, ...others) {
-  console.log('경시대회 최종 결과');
-  console.log(`대상: ${first}`);
-  console.log(`금상: ${second}`);
-  for (const other of others) {
-    console.log(`참가자: ${other}`);
-  }
-}
+let x;
+x = 3;
 
-printRank('Chung', 'Ji', 'Hyun', 'Jung', 'Hak');
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
