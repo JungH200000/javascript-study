28ba68145fb99ae1bef22aed3a32f5e77a3b53b3
JungH200000 <jhpaxk02@naver.com>
2025-10-05 13:36:12 +0900

    docs: comment 추가

--- src/js04-modern-js/c5-Array/ch02-할 일 정리/index.js
+++ src/js04-modern-js/c5-Array/ch02-할 일 정리/index.js
@@ -1,15 +1,15 @@
 function createTodo(value, order) {
   // value: 할 일에 대한 데이터를 받음
   // order: 할 일 목록의 순서를 받아 화면에 추가
-  const list = document.querySelector('.list');
-  const node = document.createElement('li');
+  const list = document.querySelector('.list'); // '.list' 클래스를 가진 요소를 찾아 'list' 변수에 저장
+  const node = document.createElement('li'); // 새로운 '<li>' 요소를 만듦
   if (value.isClear) {
-    node.classList.add('item', 'done');
+    node.classList.add('item', 'done'); // '<li>'에 'item'과 'done' 클래스 한 번에 추가
   } else {
-    node.classList.add('item');
+    node.classList.add('item'); // '<li>'에 'item' 클래스 추가
   }
-  node.textContent = `${order}. ${value.title}`;
-  list.appendChild(node);
+  node.textContent = `${order}. ${value.title}`; // <li>의 텍스트 내용을 설정
+  list.appendChild(node); // 생성한 <li>를 '.list' 요소의 마지막 자식으로 추가 -> 실제 화면에 보임
 }
 
 const data = [
