c127d9c6355f9a2135a69b7e498021c4b52f9655
JungH200000 <jhpaxk02@naver.com>
2025-10-08 23:43:47 +0900

    delete

--- study.js
+++
@@ -1,9 +0,0 @@
-// # Spread 구문 (Spread Syntax)
-
-const webPublishing = ['HTML', 'CSS'];
-const interativeWeb = [...webPublishing];
-
-interativeWeb.push('JavaScript');
-
-console.log(webPublishing);
-console.log(interativeWeb);
